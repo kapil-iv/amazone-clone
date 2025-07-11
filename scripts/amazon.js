bf20f22f66735af95181df9e2b7a9b6a0c1af775
kapil <rawat@gmail.com>
2025-07-12 02:49:06 +0530

    make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -125,6 +125,21 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    const cartHtml = ` <a class="orders-link header-link" href="orders.html">
+  <span class="returns-text">Returns</span>
+  <span class="orders-text">& Orders</span>
+  </a>
+  
+  <a class="cart-link header-link" href="checkout.html">
+  <img class="cart-icon" src="images/icons/cart-icon.png">
+  <div class="cart-quantity js-cart-quantity">${cartQuantity}</div>
+  <div class="cart-text">Cart</div>
+  </a> `;
+    document.querySelector(".amazon-header-right-section").innerHTML = cartHtml;
   });
 });
